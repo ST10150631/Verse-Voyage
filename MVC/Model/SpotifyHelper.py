9e05a10b5ca611c33020c8e4b2aad653c1707012
Mike_Turner <st10150631@vcconnect.edu.za>
2025-02-11 20:39:49 +0200

    Now retrives access token from spotify

--- MVC/Model/SpotifyHelper.py
+++ MVC/Model/SpotifyHelper.py
@@ -1,7 +1,28 @@
 from dotenv import load_dotenv
 import os
+import base64
+import json
+from requests import post
 load_dotenv()
 ## retrives the client data from the env file 
 client_id = os.getenv("Spotify_CLIENT_ID")
 client_secret = os.getenv("Spotify_CLIENT_SECRET")
+## getsb client id and concatinates to client secret in order to obtain the authorization token 
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes =  auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_bytes), "utf-8")
 
+    url = "https://accounts.spotify.com/api/token"
+    headers = {
+        "Authorization": "Basic " + auth_base64,
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+token = get_token()
+print(token)
